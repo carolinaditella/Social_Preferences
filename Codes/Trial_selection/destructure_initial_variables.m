d3a1c20eb0a25d5d4df1e0fd6612762fd3e1dad6
carolinaditella <44875216+carolinaditella@users.noreply.github.com>
2018-11-18 21:36:51 +0000

    Update and rename Codes/destructure_initial_variables.m to Codes/Trial_selection/destructure_initial_variables.m

--- Codes/Trial_selection/destructure_initial_variables.m
+++ Codes/Trial_selection/destructure_initial_variables.m
@@ -5,9 +5,5 @@ fitmodels = InitialVars.fitmodels;
 nfmodels = InitialVars.nfmodels;
 maxnumpars = InitialVars.maxnumpars;
 nparshat_vec = InitialVars.nparshat_vec;
-%partners = InitialVars.partners;
-%n_partners = InitialVars.n_partners;
-%s_n_list = InitialVars.s_n_list;
-%n_subjects = InitialVars.n_subjects;
 
 end
