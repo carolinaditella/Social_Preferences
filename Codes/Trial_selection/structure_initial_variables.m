353d1a0a83db6c4a0227e8cd7d825a5e371ced04
carolinaditella <44875216+carolinaditella@users.noreply.github.com>
2018-11-18 21:37:25 +0000

    Update and rename Codes/structure_initial_variables.m to Codes/Trial_selection/structure_initial_variables.m

--- Codes/Trial_selection/structure_initial_variables.m
+++ Codes/Trial_selection/structure_initial_variables.m
@@ -4,10 +4,6 @@ InitialVars.fitmodels = fitmodels;
 InitialVars.nfmodels = nfmodels;
 InitialVars.maxnumpars = maxnumpars;
 InitialVars.nparshat_vec = nparshat_vec;
-%InitialVars.partners = partners; 
-%InitialVars.n_partners = n_partners;
-%InitialVars.s_n_list = s_n_list;
-%InitialVars.n_subjectcts = n_subjects;
 
 end
 
