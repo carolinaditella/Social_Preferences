a5ededc345ef6974ab6a079686031938c414cbb5
carolinaditella <44875216+carolinaditella@users.noreply.github.com>
2018-11-18 21:52:30 +0000

    Add files via upload

---
+++ Codes/Analysis/destructure_analysis_variables.m
@@ -0,0 +1,9 @@
+function [partners,n_partners,s_n_list,n_subjects] = destructure_analysis_variables(AnalysisVars)
+
+
+partners = AnalysisVars.partners;
+n_partners = AnalysisVars.n_partners;
+s_n_list = AnalysisVars.s_n_list;
+n_subjects = AnalysisVars.n_subjects;
+
+end
---
+++ Codes/Analysis/structure_analysis_variables.m
@@ -0,0 +1,8 @@
+function [AnalysisVars] = structure_analysis_variables_(partners,n_partners,s_n_list,n_subjects)
+
+AnalysisVars.partners = partners; 
+AnalysisVars.n_partners = n_partners;
+AnalysisVars.s_n_list = s_n_list;
+AnalysisVars.n_subjects = n_subjects;
+end
+
